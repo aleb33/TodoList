65f051731fd740f3fbd374e17c91422297d9eb6d
Alexandre Bousquet <alexandre.bousquet@etu.u-bordeaux.fr>
2022-03-25 10:34:37 +0100

    Ajout de toutes les routes possibles

--- js/routes.js
+++ js/routes.js
@@ -1,11 +1,34 @@
 const express = require('express');
 const app = express();
 const router = express.Router();
-app.use(express.static('../source/'));
-const source='../source/';
+app.use(express.static('./source/'));
+const source='./source/';
 
-app.get('',(req,res) => {
+
+app.get('/',(req,res) => {
   res.sendFile(source+'index.html',{root: source, mime:'text/css'});
+  //__dirname : It will resolve to your project folder.
+});
+
+app.get('/formulaire',(req,res) => {
+  res.sendFile('formulaire.html',{root: source, mime:'text/css'});
+  //__dirname : It will resolve to your project folder.
+});
+
+app.get('/listing_groupe',(req,res) => {
+  res.sendFile('listing_groupe.html',{root: source, mime:'text/css'});
+  //__dirname : It will resolve to your project folder.
+});
+app.get('/listing_tache',(req,res) => {
+  res.sendFile('listing_tache.html',{root: source, mime:'text/css'});
+  //__dirname : It will resolve to your project folder.
+});
+app.get('/modifier_groupe',(req,res) => {
+  res.sendFile('modifier_groupe.html',{root: source, mime:'text/css'});
+  //__dirname : It will resolve to your project folder.
+});
+app.get('/modifier_tache',(req,res) => {
+  res.sendFile('modifier_tache.html',{root: source, mime:'text/css'});
   //__dirname : It will resolve to your project folder.
 });
 //add the router
